16d93ef56468e1803966788005d76dc0a4c7aef9
ChristianOestraich <christian.oestraich@universo.univates.br>
2020-10-12 22:08:29 -0300

    Aprendendo a usar funcoes data

---
+++ Date/exemplo-01.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo date("d/m/Y H:i:s",1602550893);
+
+    echo "<br>";
+
+    echo time();
+
+?>
---
+++ Date/exemplo-02.php
@@ -0,0 +1,5 @@
+<?php
+    $ts = strtotime("2001-09-11");
+
+    echo date("1, d/m/Y", $ts);
+?>
