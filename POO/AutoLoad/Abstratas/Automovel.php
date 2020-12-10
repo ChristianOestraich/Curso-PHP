73f82d14d8ebac87c8792e685628f757b22f692c
ChristianOestraich <christian.oestraich@universo.univates.br>
2020-12-10 20:54:57 -0300

    Auto load

---
+++ POO/AutoLoad/Abstratas/Automovel.php
@@ -0,0 +1,27 @@
+<?php
+interface Veiculo
+{
+
+    public function acelerar($velocidade);
+    public function freiar($velocidade);
+    public function trocarMarcha($marcha);
+}
+
+abstract class Automovel implements Veiculo
+{
+    public function acelerar($velocidade)
+    {
+
+        echo "O veiculo acelerou ate: " . $velocidade . " km/h";
+    }
+    public function freiar($velocidade)
+    {
+
+        echo "O veiculo freio a: " . $velocidade . " km/h";
+    }
+    public function trocarMarcha($marcha)
+    {
+
+        echo "O veiculo trocou para a marcha: " . $marcha;
+    }
+}
